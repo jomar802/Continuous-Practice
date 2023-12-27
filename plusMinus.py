c56ffb95286b2a2280432d55fdd15f09e3e046b9
Jomar <jomar.alayon@upr.edu>
2023-12-27 15:32:31 -0400

    added conditional; small fix

--- plusMinus.py
+++ plusMinus.py
@@ -15,20 +15,24 @@ import sys
 def plusMinus(arr):
     # Write your code here
     
-    arr_len = len(arr)
-    positive = 0
-    negative = 0
-    for i in range(0, arr_len):
-        if arr[i] > 0:
-            positive = positive + 1
-        if arr[i] < 0:
-            negative = negative + 1
-        if arr[i] == 0:
-            zero = zero + 1
-            
-    ratio_pos = positive / arr_len
-    ratio_neg = negative / arr_len
-    ratio_zero = zero / arr_len
+    if len(arr) <= 0:
+        return 0
+    else:
+        arr_len = len(arr)
+        positive = 0
+        negative = 0
+        zero = 0
+        for i in range(0, arr_len):
+            if arr[i] > 0:
+                positive = positive + 1
+            if arr[i] < 0:
+                negative = negative + 1
+            if arr[i] == 0:
+                zero = zero + 1
+                
+        ratio_pos = positive / arr_len
+        ratio_neg = negative / arr_len
+        ratio_zero = zero / arr_len
     
     print("positive:",ratio_pos)
     print("negative:",ratio_neg)
