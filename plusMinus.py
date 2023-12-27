fc5dd7521234e29cd5daebf13d616b122c94ff9c
Jomar <jomar.alayon@upr.edu>
2023-12-27 15:23:27 -0400

    plusMinus done

---
+++ plusMinus.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    
+    arr_len = len(arr)
+    positive = 0
+    negative = 0
+    for i in range(0, arr_len):
+        if arr[i] > 0:
+            positive = positive + 1
+        if arr[i] < 0:
+            negative = negative + 1
+        if arr[i] == 0:
+            zero = zero + 1
+            
+    ratio_pos = positive / arr_len
+    ratio_neg = negative / arr_len
+    ratio_zero = zero / arr_len
+    
+    print("positive:",ratio_pos)
+    print("negative:",ratio_neg)
+    print("zeroes:",ratio_zero)
+    
+    
+
+x = [2,4,5,0,-1,-2]
+plusMinus(x)
+    
+            
+    
+    
+    
+
